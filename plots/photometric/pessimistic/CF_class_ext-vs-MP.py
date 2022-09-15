71269bf1aadd7759475179f5c82fd61fb46011e1
lesgourg <lesgourg@physik.rwth-aachen.de>
2022-09-15 15:14:39 +0200

    updated python scripts for plotting photo/pess class_ext

--- plots/photometric/pessimistic/CF_class_ext-vs-MP.py
+++ plots/photometric/pessimistic/CF_class_ext-vs-MP.py
@@ -3,7 +3,7 @@ import os, sys, argparse
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append('../../')
 
-parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter) 
+parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
 parser.add_argument('--error-only',action='store_true',dest='error_only',
                     help='    Plot error comparions plots only',
                     default=False)
@@ -13,7 +13,7 @@ error_only = args.error_only
 from plot_master import plotter
 
 fish_files =  [
-    '../../../results/cosmicfish_external/photometric/pessimistic/CosmicFish_v0.9_w0wa_external_class-Pessimistic-3PT_HP_WLGCph_fishermatrix.txt',
+    '../../../results/cosmicfish_external/photometric/pessimistic/CosmicFish_v0.9_w0wa_external_class-Pessimistic-3PT_LP_WLGCph_fishermatrix.txt',
     '../../../results/montepython_fisher/photometric/pessimistic/fisher.mat'
               ]
 
@@ -22,4 +22,4 @@ labels = [r'CF_ext_class XCph pess',
 
 cutnames=['Omegam', 'Omegab', 'ns', 'h','sigma8','w0', 'wa', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9', 'b10','AIA', 'etaIA']
 
-plotter(fish_files=fish_files,labels=labels,pars=cutnames,error_only=error_only) 
+plotter(fish_files=fish_files,labels=labels,pars=cutnames,error_only=error_only)
