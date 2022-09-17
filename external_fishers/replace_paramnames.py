037326dc8d554dea7901a1770db5b709ff800b8a
Santiago Casas <santiagocasas@gmail.com>
2022-09-17 15:51:03 +0200

    updates for the 4-code comparison in 4 cases

---
+++ external_fishers/replace_paramnames.py
@@ -0,0 +1,25 @@
+import glob
+import sys
+import os
+
+path_to_change = sys.argv[1]
+dirname = os.path.realpath(path_to_change)
+print(dirname)
+os.chdir(dirname)
+
+pars_old=['aIA', 'bIA', 'eIA']
+pars_new=['AIA', 'betaIA', 'etaIA']
+
+
+for f in glob.glob("*.paramnames"):
+    print(f)
+    with open(f, "r") as inputfile:
+        newText = inputfile.read()
+        for po, pn in zip(pars_old, pars_new):
+    	    newText = newText.replace(po, pn)
+        newText = newText.replace('b10    b10   1.743', 'b10    b10    1.743')
+    print(newText)
+    with open(f, "w") as outputfile:
+        outputfile.write(newText)
+
+print("***Finished replacing strings***")
---
+++ plots/photometric/pessimistic/4codes-CF_class_camb-vs-ISTF-vs-MP.py
@@ -0,0 +1,33 @@
+import os, sys, argparse
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append('../../')
+
+parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('--error-only',action='store_true',dest='error_only',
+                    help='    Plot error comparions plots only',
+                    default=False)
+args = parser.parse_args()
+error_only = args.error_only
+
+from plot_master import plotter
+
+fish_files =  [
+    '../../../results/cosmicfish_internal/photometric/pessimistic/CosmicFish_v0.9_w0wa_internal_class-Pessimistic-3PT_WLGCph_fishermatrix.txt',
+    '../../../results/cosmicfish_internal/photometric/pessimistic/CosmicFish_v0.9_w0wa_internal_camb-Pessimistic-3PT_WLGCph_fishermatrix.txt',
+    '../../../results/montepython_fisher/photometric/pessimistic/fisher.mat',
+    '../../../../fisher_for_public/All_Results/pessimistic/flat/EuclidISTF_GCph_WL_XC_w0wa_flat_pessimistic.txt'
+              ]
+
+labels = [
+         r'CF_int_class',
+         r'CF_int_camb',
+         r'MP',
+         r'ISTF'
+        ]
+
+cutnames=['Omegam', 'Omegab', 'ns', 'h','sigma8','w0', 'wa', 'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9', 'b10','AIA', 'etaIA']
+
+compare_errors_dict={'ncol_legend':4, 'legend_title':'XCph pess'}# 'legend_title_fontsize':16} 
+
+plotter(fish_files=fish_files,labels=labels,pars=cutnames,error_only=error_only, compare_errors_dict=compare_errors_dict)
